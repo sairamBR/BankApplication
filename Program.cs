0fc07ca9500b42fb97825e83d63fdb6d00bcdf7c
sairamBR <sairam.au18@bitsathy.ac.in>
2021-02-23 23:35:24 +0530

    SMALL BANK PROCESS APLLICATION

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BankApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var account = new BankAccount("sairam", 10000);
+          Console.WriteLine("Wellcome SANA SMALL FINANCE BANK PVT.Ltd");
+          Console.WriteLine($"Account No: {account.Number}");
+          Console.WriteLine($"Owner of the Account: {account.Owner}");
+          Console.WriteLine($"Account Balance:  {account.Balance}");
+        }
+    }
+}
