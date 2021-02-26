91460a0e06db5e6d5d0d12f0201b89fb7a3009a2
sairamBR <sairam.au18@bitsathy.ac.in>
2021-02-26 22:55:22 +0530

    Added The Withdrawal and Deposit features

--- BankAcconts.cs
+++ BankAcconts.cs
@@ -24,19 +24,37 @@ namespace BankApplication
 
         private List<Transaction> allTransactions = new List<Transaction>();
 
-        public BankAccount(string name)
+        public BankAccount(string name, decimal initialBalance)
         {
             this.Owner = name;
             this.Number = accountNumberSeed.ToString();
             accountNumberSeed++;
+
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
         }
 
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be postive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
         }
 
         public void MakeWithdrawal(decimal amount, DateTime date, string note)
         {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount Withdrawal must be postive");
+            }
+            if(Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not Sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,11 +6,19 @@ namespace BankApplication
     {
         static void Main(string[] args)
         {
-          var account = new BankAccount("ShamiliPriya");
+          var account = new BankAccount("ShamiliPriya", 10000);
           Console.WriteLine("Wellcome SANA SMALL FINANCE BANK PVT.Ltd");
           Console.WriteLine($"Account No: {account.Number}");
           Console.WriteLine($"Owner of the Account: {account.Owner}");
           Console.WriteLine($"Account Balance:  {account.Balance}");
+
+          Console.WriteLine("-------------------------------------------------------------------------");
+          account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+          Console.WriteLine("Balance after withdrawal on "+ DateTime.Now + "    "+ account.Balance);
+          account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+          Console.WriteLine("Balance After Deposit on " + DateTime.Now + "       "+ account.Balance);
+          Console.WriteLine("-------------------------------------------------------------------------");
+          Console.WriteLine("Current Balance                                    " +account.Balance);
         }
     }
 }
